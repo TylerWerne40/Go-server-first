6504fbfd67fa15cfd5ea92460b33a12f69570380
Tyler <TylerWerne40@gmail.com>
2024-05-17 04:05:12 -0600

    Error gone. Added waitgroups to make the program wait until goroutines finished.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"os"
 	"sync"
+	"time"
 )
 
 func server(group *sync.WaitGroup) {
@@ -18,19 +19,20 @@ func server(group *sync.WaitGroup) {
 		fmt.Println(err)
 		return
 	}
-	for {
-		// accept a connection
-		c, err := ln.Accept()
-		if err != nil {
-			fmt.Println("Error accepting: ", ln.Addr(), err)
-			fmt.Println(err)
-		}
-		// handle the connection
-		go handleServerConnection(c)
+	// accept a connection
+	c, err := ln.Accept()
+	if err != nil {
+		fmt.Println("Error accepting: ", ln.Addr(), err)
+		fmt.Println(err)
 	}
+	// handle the connection
+	wg2 := new(sync.WaitGroup)
+	wg2.Add(1)
+	go handleServerConnection(c, wg2)
+	wg2.Wait()
 }
 
-func handleServerConnection(c net.Conn) {
+func handleServerConnection(c net.Conn, group *sync.WaitGroup) {
 	//receive the message
 	var msg string
 	err := gob.NewDecoder(c).Decode(&msg)
@@ -56,14 +58,15 @@ func handleServerConnection(c net.Conn) {
 
 			}
 		}
+		group.Done()
 	}()
 }
 
 func client(group *sync.WaitGroup) {
 	// connect to the server
-	defer group.Done()
 	c, err := net.Dial("tcp", ":8080")
 	if err != nil {
+		fmt.Println("Client Error connecting:", err.Error())
 		fmt.Println(err)
 		return
 	}
@@ -80,7 +83,9 @@ func client(group *sync.WaitGroup) {
 				fmt.Println(err)
 				fmt.Println("Couldn't close connection")
 			}
+
 		}
+		group.Done()
 	}()
 	// send the message
 	fmt.Print("What shall we send? ")
@@ -102,11 +107,13 @@ func client(group *sync.WaitGroup) {
 }
 
 func main() {
-	var wg sync.WaitGroup
+	wg := new(sync.WaitGroup)
+
 	wg.Add(1)
-	go server(&wg)
+	go server(wg)
 	wg.Add(1)
-	go client(&wg)
+	time.Sleep(time.Second)
+	go client(wg)
 	var input string
 	wg.Wait()
 	fmt.Print("End? ")
